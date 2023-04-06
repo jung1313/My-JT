875cea8443b2343e0332a7136c36ea1c7d43cb4b
Jung1133 <112536367+jung1313@users.noreply.github.com>
2023-04-06 15:30:31 +0900

    fixed0406

--- pyper.py
+++ pyper.py
@@ -5,30 +5,17 @@ import openpyxl
 import os
 import glob
 from pathlib import Path
-"""
-aa= pag.position()
-print(aa)
-"""
-x = 1540
-y = 290
-s = 0
 
-#  座標
-
-#  英語  x 406 y 375
-#  ベトナム語 418 394
-#　インドネシア語 421 520
-#　とりあえずこのコードはスライド一項目につきるものである。
-
-
-def pushget():
-    x = 1540
-    y = 290
-    pag.click(x, y, duration=0.5) 
+def paste_translate(xx,yy):
+    global ls_num
+    ya=290+ls_num
+    pag.click(x=1538,y=ya, duration=0.5) 
     time.sleep(0.8) #編集画面入るまでの時間
     pag.click(1843,325,duration = 0.5) # Translation 選択
     pag.click(454,271,duration=1) # 言語リスト選択
-    pag.click(370,372,duration=1) # 翻訳する言語位置選択
+
+    pag.click(xx,yy,duration=1) # 翻訳する言語位置選択
+
     pag.doubleClick(360,372,duration=1)
     pag.hotkey('ctrl','a',duration=1)
     pag.press('del')
@@ -45,56 +32,82 @@ def pushget():
     pag.click(70,118,duration = 0.5)
     time.sleep(1)
     pag.click(57,101,duration = 0.5)
-    y = y+33
-   
+    
 
 
-def MouseMove():
-    pag.moveTo(1540,290,duration=0.5)#default
-    pag.moveTo(1540,323,duration=0.5) #33
-    pag.moveTo(1540,357,duration=0.5)
-    pag.moveTo(1540,392,duration=0.5)
-    pag.moveTo(1540,427,duration=0.5)
-    pag.moveTo(1540,459,duration=0.5)
-    pag.moveTo(1540,491,duration=0.5)
-    pag.moveTo(1540,523,duration=0.5)
-    pag.moveTo(1540,555,duration=0.5)
-    pag.moveTo(1540,duration=0.5)
-    pag.moveTo(1540,430,duration=0.5)
-    pag.moveTo(1540,430,duration=0.5)
+file_path1 = "C:\\Users\\ウヨン\\Downloads\\製造業フレーズ_0316_翻訳入り_v4 チェック済1.xlsx"
 
+def select_sheet(s):
+    
+    start_row = 4
+    lesson_name=[]
+    wb= openpyxl.load_workbook(file_path1)
+    ws=wb.worksheets[s]
+    while start_row < 201:
 
+        cell = ws.cell(start_row,1)
 
-file_path1 = "C:\\Users\\ウヨン\\Downloads\\介護フレーズ_0315_翻訳入り_v4.xlsx"
+        if cell.value is not None:
+
+            lesson_name.append(cell.value)
+        start_row += 1
+    return lesson_name
+    wb.save(i)
+
 
 wb = openpyxl.load_workbook(file_path1,data_only=True)
-for ws in wb.worksheets:
-    start_row = 4
-    start_column = 6
-    dif_col = 3
-    vt_col = 10
-    quote = []
-    B = "B:"
-    A = "A:"
-    while not ws.cell(start_row , start_column).value is None:
-        #print(ws.cell(start_row,vt_col).value)
-        #if (ws.cell(row = start_row , column = start_column).value ==  ws.cell(row = start_row + 1 , column = start_column).value):
-        quote.extend([B+ws.cell(start_row,start_column).value , ws.cell(start_row,vt_col).value , A+ws.cell(start_row,start_column).value , ws.cell(start_row,vt_col).value])
-        
+ws=wb.worksheets[0]
+start_row = 4
+start_column = 6
+dif_col = 3 #바뀌는 열
+eg_col = 9
+vt_col = 10
+id_col = 11
+quote_en = []
+quote_vi = []
+quote_id = []
+B = "B:"
+A = "A:"
+length = select_sheet(0)
+
+for iii in range(len(length)): 
+
+    while not ws.cell(start_row , start_column).value is None :
+
+        #英語
+        quote_en.extend([B+ws.cell(start_row,start_column).value , ws.cell(start_row,eg_col).value , A+ws.cell(start_row,start_column).value , ws.cell(start_row,eg_col).value])
+        #ベトナム語
+        quote_vi.extend([B+ws.cell(start_row,start_column).value , ws.cell(start_row,vt_col).value , A+ws.cell(start_row,start_column).value , ws.cell(start_row,vt_col).value])
+        #インドネシア語
+        quote_id.extend([B+ws.cell(start_row,start_column).value , ws.cell(start_row,id_col).value , A+ws.cell(start_row,start_column).value , ws.cell(start_row,id_col).value])
+
         if (ws.cell(row = start_row , column = dif_col).value !=  ws.cell(row = start_row + 1 , column = dif_col).value):
-            #print("-----------------------")
-            quoteline = "\n".join(quote)
-            pyperclip.copy(quoteline)
-            Paste = pyperclip.paste()
-            #print(Paste)
-
-            pushget()
-
-
-            #i = i+1
-          
-            quote = []
-        start_row += 1
+            
+            #英語
+            
+            quoteline_en = "\n".join(quote_en)
     
-    wb.save(file_path1) 
+            pyperclip.copy(quoteline_en)
+            ls_num = (iii)*33
+            paste_translate(406,375)
+        
+            #ベトナム語
+            quoteline_vi = "\n".join(quote_vi)
+            pyperclip.copy(quoteline_vi)
+            ls_num = (iii)*33
+            paste_translate(418,394)
+        
+            #インドネシア語
+            quoteline_id = "\n".join(quote_id)
+            pyperclip.copy(quoteline_id)
+            ls_num = (iii)*33
+            paste_translate(421,520)
+            
+            quote_en = []
+            quote_vi = []
+            quote_id = []
+
+        start_row += 1
+
+        #wb.save(file_path1) 
 
